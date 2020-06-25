683b17f3aa4cdc4c2355237334e138f2edba8220
gceboh <gceboh@users.noreply.github.com>
2020-06-25 21:10:32 +0800

    load "zoom factor" dynamically from ini config

--- code/server/config.py
+++ code/server/config.py
@@ -1,7 +1,18 @@
+import configparser
+
+# get ini config parser
+ini_config = configparser.ConfigParser()
+
+
 device_A = '/gpu:0'
 device_B = '/gpu:0'
 multiple_process = True
 debugging = False
 
-# tweak_sketch_resolution_factor=1 # default input sketch size (short edge=1024px), used by official version
-tweak_sketch_resolution_factor=0.5 # Scale the input sketch to 50% (0.5) of the default size (short edge=512px)
+
+# load "zoom factor" from ini config file
+def get_sketch_resolution_factor():
+    ini_path = 'config.ini'
+    ini_config.read(ini_path) # read ini config
+    resolution_factor = ini_config.getfloat('general','sketch_zoom_factor')
+    return resolution_factor
--- code/server/server.py
+++ code/server/server.py
@@ -45,6 +45,10 @@ painting_pool = []
 
 def handle_sketch_upload_pool():
     if len(sketch_upload_pool) > 0:
+        # print input sketch's resolution
+        sketch_real_resolution=int(1024*get_sketch_resolution_factor())
+        print('Currently all input sketch will be resized to ' + str(sketch_real_resolution) + 'px')
+        
         room, sketch, method = sketch_upload_pool[0]
         del sketch_upload_pool[0]
         room_path = 'game/rooms/' + room
@@ -55,7 +59,7 @@ def handle_sketch_upload_pool():
         else:
             improved_sketch = sketch.copy()
             # tweak the input sketch's resolution by factor
-            tweaked_input_resolution=int(512 * tweak_sketch_resolution_factor)
+            tweaked_input_resolution=int(512 * get_sketch_resolution_factor())
             improved_sketch = min_resize(improved_sketch, tweaked_input_resolution)
             improved_sketch = cv_denoise(improved_sketch)
             improved_sketch = sensitive(improved_sketch, s=5.0)
@@ -92,7 +96,7 @@ def handle_painting_pool():
         room_path = 'game/rooms/' + room
         print('processing painting in ' + room_path)
         # tweak the input sketch's resolution by factor
-        tweaked_input_resolution=int(64 * tweak_sketch_resolution_factor)
+        tweaked_input_resolution=int(64 * get_sketch_resolution_factor())
         sketch_1024 = k_resize(sketch, tweaked_input_resolution)
         if os.path.exists(room_path + '/sketch.de_painting.jpg') and method == 'rendering':
             vice_sketch_1024 = k_resize(cv2.imread(room_path + '/sketch.de_painting.jpg', cv2.IMREAD_GRAYSCALE), 64)
